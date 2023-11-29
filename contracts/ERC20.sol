041b2e47297012770b91d2ee9d40b33738edbc35
John Bergschneider <jhb10c@my.fsu.edu>
2023-11-28 19:08:23 -0500

    try

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -31,7 +31,7 @@ import "./../contracts/IERC20Metadata.sol";
  * functions have been added to mitigate the well-known issues around setting
  * allowances. See {IERC20-approve}.
  */
-contract ERC20 is IERC20, IERC20Metadata {
+contract ERC20 {
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
